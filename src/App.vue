1bf1ff86a11273daf53c0626289bce419d9b3933
JakubChorzepa <jakubchorzepa@gmail.com>
2021-04-28 16:36:26 +0200

    Deleted issues after merge

--- src/App.vue
+++ src/App.vue
@@ -1,11 +1,6 @@
 <template>
-<<<<<<< HEAD
-  <div class="nav">
-    <router-link to="/">Home</router-link>
-=======
   <div id="nav">
     <router-link to="/">Spacer</router-link> |
->>>>>>> simple-search
     <router-link to="/about">About</router-link>
     <router-view />
   </div>
